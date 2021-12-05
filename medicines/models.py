e68f68471e064d41da4abbb9338607ce42bb7165
saitako <saitako@outlook.com>
2021-12-05 11:49:34 +0800

    add/f-y-medicines

--- medicines/models.py
+++ medicines/models.py
@@ -1,16 +1,20 @@
 from django.db import models
 
-class Medicines(models.Model):
+# class Medicines(models.Model):
+#     bapomofo = models.CharField(max_length=8)
+#     name = models.CharField(max_length=256)
+#     info = models.JSONField(default=dict)
+
+
+class FangMedicines(models.Model): # Composed
     bapomofo = models.CharField(max_length=8)
     name = models.CharField(max_length=256)
     info = models.JSONField(default=dict)
 
 
-# class FangMedicines(models.Model): # Composed
-#     ...
-
-
-# class YaoMedicines(models.Model): # Single
-#     ...
+class YaoMedicines(models.Model): # Single
+    bapomofo = models.CharField(max_length=8)
+    name = models.CharField(max_length=256)
+    info = models.JSONField(default=dict)
 
     
