c657991e0f544d157c811d7000d8562a1aa9aa77
Mukhammed123 <muhammedmusaaa@gmail.com>
2022-07-08 11:04:32 +0800

    changed page size

--- medicines/views.py
+++ medicines/views.py
@@ -22,7 +22,7 @@ from .serializers import (
 
 
 class StandardResultsSetPagination(PageNumberPagination):
-    page_size = 10
+    page_size = 100
     page_size_query_param = 'page_size'
     max_page_size = 100
 
