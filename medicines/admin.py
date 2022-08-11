212235f7186f750ccb15c2b4d7c6025ca9efaceb
Mukhammed123 <muhammedmusaaa@gmail.com>
2022-08-11 16:39:54 +0800

    added treatments to admin page

--- medicines/admin.py
+++ medicines/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Medicine, MedicineRecord, Decoction, DecoctionComponents, DecoctionRecord
+from .models import Medicine, MedicineRecord, Decoction, DecoctionComponents, DecoctionRecord, InjuryTreatment, InjuryTreatmentRecord
 
-admin.site.register([Medicine, MedicineRecord, Decoction, DecoctionComponents, DecoctionRecord])
+admin.site.register([Medicine, MedicineRecord, Decoction, DecoctionComponents, DecoctionRecord, InjuryTreatment, InjuryTreatmentRecord])
 
